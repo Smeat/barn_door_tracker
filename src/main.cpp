07e5cf737b204e934cf472b8d618520f9bb2bba7
Kevin <smeatsan@gmail.com>
2020-04-25 11:05:19 +0200

    Set the license to GPLv3

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
-#include "HardwareSerial.h"
+/*
+ * Copyright (c) 2020 smeat.
+ *
+ * This file is part of barn_door_tracker 
+ * (see https://github.com/Smeat/barn_door_tracker).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <Arduino.h>
 #include <CheapStepper.h>
 #include <Bounce2.h>
